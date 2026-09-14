42827524f9d8ff3fdff9e1717b32828534b3d9eb
mdmahfuj-cse <mdmahfuj.cse@gmail.com>
2026-09-14 23:26:38 +0600

    first commit

--- index.js
+++ index.js
@@ -14,14 +14,3 @@ app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <div>Hello World</div>,
-  },
-]);
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>,
